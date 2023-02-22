a1837d5e375343cb3b3c4b2746c316d1144e70ff
Mikio Hirabayashi <hirarin@gmail.com>
2023-02-22 19:44:27 +0900

    Add a comment.

--- tkrzw-doc.py
+++ tkrzw-doc.py
@@ -900,7 +900,7 @@ class DBM:
     """
     Searches the database and get keys which match a pattern.
 
-    :param mode: The search mode.  "contain" extracts keys containing the pattern.  "begin" extracts keys beginning with the pattern.  "end" extracts keys ending with the pattern.  "regex" extracts keys partially matches the pattern of a regular expression.  "edit" extracts keys whose edit distance to the UTF-8 pattern is the least.  "editbin" extracts keys whose edit distance to the binary pattern is the least.
+    :param mode: The search mode.  "contain" extracts keys containing the pattern.  "begin" extracts keys beginning with the pattern.  "end" extracts keys ending with the pattern.  "regex" extracts keys partially matches the pattern of a regular expression.  "edit" extracts keys whose edit distance to the UTF-8 pattern is the least.  "editbin" extracts keys whose edit distance to the binary pattern is the least.  "containcase", "containword", and "containcaseword" extract keys considering case and word boundary.  Ordered databases support "upper" and "lower" which extract keys whose positions are upper/lower than the pattern. "upperinc" and "lowerinc" are their inclusive versions.
     :param pattern: The pattern for matching.
     :param capacity: The maximum records to obtain.  0 means unlimited.
     :return: A list of string keys matching the condition.
