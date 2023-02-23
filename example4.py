560580b8757aa0c79007d566ad02df4fb23e60e8
Mikio Hirabayashi <hirarin@gmail.com>
2023-02-23 22:55:47 +0900

    Modify comments.

--- example4.py
+++ example4.py
@@ -48,8 +48,6 @@ dbm.Process("doc-3", lambda key, value: print(key, value), False)
 dbm.ProcessMulti([
     ("doc-4", lambda key, value: "Tokyo Go!"),
     ("doc-5", lambda key, value: "Japan Go!")], True)
-
-# Modifies multiple records at once.
 dbm.ProcessMulti([("doc-4", Lower), ("doc-5", Lower)], True)
 
 # Checks the whole content.
