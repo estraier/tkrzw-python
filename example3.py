1ef645adcf7d95255b4d49ee5a5c371be2cddc8c
Mikio Hirabayashi <hirarin@gmail.com>
2023-02-23 22:39:38 +0900

    Modify comments.

--- example3.py
+++ example3.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 #--------------------------------------------------------------------------------------------------
-# Example for the asynchronous database
+# Example for the asynchronous API
 #
 # Copyright 2020 Google LLC
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
